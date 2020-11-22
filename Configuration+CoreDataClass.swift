1d54735c3ba5334f854cb8a930698c6eca193fff
Brandon Clark <bclark5@zagmail.gonzaga.edu>
2020-11-21 20:31:46 -0800

    Initial Commit

---
+++ Configuration+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Configuration+CoreDataClass.swift
+//  equal sounds
+//
+//  Created by Brandon Clark on 11/21/20.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Configuration)
+public class Configuration: NSManagedObject {
+
+}
