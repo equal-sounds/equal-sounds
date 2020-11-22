b2b843dcafac25a9ec22772157579a2b2fe8cb6b
Brandon Clark <bclark5@zagmail.gonzaga.edu>
2020-11-21 19:57:02 -0800

    Initial Commit

---
+++ equal sounds/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  equal sounds
+//
+//  Created by Brandon Clark on 11/21/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
