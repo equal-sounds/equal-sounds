16e792f9f474afb9554dc588dcdc76b05f447091
Brandon Clark <bclark5@zagmail.gonzaga.edu>
2020-12-14 19:59:53 -0800

    song file updated

--- equal sounds/Song.swift
+++ equal sounds/Song.swift
@@ -6,17 +6,24 @@
 //
 
 import Foundation
+import UIKit
 
 
 class Song{
     var name: String
     var artist: String
     var albumName: String
+    var duration: Int
+    var albumImage: UIImage
+    var url: URL
     
-    init(name: String, artist: String, albumName: String)
+    init(name: String, artist: String, albumName: String, duration: Int, albumImage: UIImage, url: URL)
     {
         self.name = name
         self.artist = artist
         self.albumName = albumName
+        self.duration = duration
+        self.albumImage = albumImage
+        self.url = url
     }
 }
